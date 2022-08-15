2b3ab5695af13b8cd55f91a088744c0f7391cb19
ColinLangella <colinlangella@gmail.com>
2022-08-14 20:39:44 -0400

    Added currently playing function

--- bot.py
+++ bot.py
@@ -1,12 +1,9 @@
+import keys
 import asyncio
-from time import sleep
 import discord
 from discord.ext import commands
-from discord import FFmpegPCMAudio
-import keys
 from datetime import datetime
 from searchyoutube import youtube_search
-
 
 intents = discord.Intents.default()
 intents.members = True
@@ -20,10 +17,11 @@ async def on_ready():
     print("\n=====================================================")
 
 # Music-related commands
-class Voice(commands.Cog):
+class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.queue = []
+        self.playing = None
 
     # Join voice channel
     @commands.command(name='join', aliases=['j', 'J', 'Join'])
@@ -35,9 +33,9 @@ class Voice(commands.Cog):
         if (ctx.author.voice):
             if ctx.voice_client == None:
                 voice_channel = ctx.message.author.voice.channel
-                vc = await voice_channel.connect()
+                await voice_channel.connect()
                 await ctx.send("Joined " + str(voice_channel))
-                print(" -Bot joined " + str(voice_channel) + "\n")
+                print(" -Bot joined", str(voice_channel), "\n")
             else:
                 await ctx.send("I'm already in a voice channel")
                 print(" -Bot already in a voice channel\n")
@@ -58,6 +56,7 @@ class Voice(commands.Cog):
             print(" -Bot disconnected\n")
         else:
             await ctx.send("I'm not in a voice channel")
+            print(" -Bot not in channel\n")
 
     # Play song
     @commands.command(name='play', aliases=['p', 'P', 'Play'])
@@ -66,14 +65,18 @@ class Voice(commands.Cog):
         current_time = now.strftime("%H:%M:%S")
         print("(" + current_time + ") " + str(ctx.author) + " used the command: " + str(ctx.message.content))
 
-
+        choice = ctx.message.content
+        if choice[1:5].upper() == 'PLAY':
+            choice = choice[6:len(choice)]
+        else:
+            choice = choice[3:len(choice)]
 
         # Check if the user in a channel
         if (ctx.author.voice):
 
             vc = ctx.voice_client
             # Check if the bot is already in a channel
-            if ctx.voice_client == None:
+            if vc == None:
                 voice_channel = ctx.message.author.voice.channel
                 vc = await voice_channel.connect()
                 await ctx.send("Joined " + str(voice_channel))
@@ -81,36 +84,31 @@ class Voice(commands.Cog):
             else:
                 pass
 
-            search_query = ctx.message.content[6:len(ctx.message.content)]
-            audio_url, title, length, url = youtube_search(search_query)
+            audio_url, title, length, url = youtube_search(choice)
 
-            if not vc.is_playing():
-                vc.play(discord.FFmpegPCMAudio(executable="C:/FFmpeg/ffmpeg.exe", source=audio_url))
-                await ctx.send("Now playing \"" + title + "\"")
-                print(" -Bot began playing \"" + title + "\"\n")
-                while vc.is_playing():
-                    await asyncio.sleep(.5)
+            self.queue.append((title, audio_url, length, url))
+            await ctx.send("Added \"" + title + "\" to queue")
+            print(" -Added \"" + title + "\" to queue\n")
+
+            if self.playing == None:
                 await self.play_queue(ctx, vc)
-            else:
-                self.queue.append((title, audio_url, length, url))
-                await ctx.send("Added \"" + title + "\" to queue")
-                print(" -Added \"" + title + "\" to queue\n")
-                #print("Queue: " + str(self.queue))
+
+            #print("Queue: " + str(self.queue))
 
         else:
             await ctx.send("You must be in a voice channel for me to join")
             print(" -User not in channel\n")
 
     async def play_queue(self, ctx, vc):
-        if len(self.queue) == 0:
-            return
-        title, audio_url, time, url = self.queue.pop(0)
-        vc.play(discord.FFmpegPCMAudio(executable="C:/FFmpeg/ffmpeg.exe", source=audio_url))
-        await ctx.send("Now playing \"" + title + "\" (" + url + ")")
-        print(" -Bot began playing \"" + title + "\"\n")
-        while vc.is_playing():
-            await asyncio.sleep(.5)
-        await self.play_queue(ctx, vc)
+        while len(self.queue) != 0:
+            title, audio_url, time, url = self.queue.pop(0)
+            self.playing = title
+            vc.play(discord.FFmpegPCMAudio(executable="C:/Users/colin/Documents/ffmpeg/bin/ffmpeg.exe", source=audio_url))
+            await ctx.send("Now playing \"" + title + "\" (" + url + ")")
+            print(" -Bot began playing \"" + title + "\"\n")
+            while vc.is_playing():
+                await asyncio.sleep(.5)
+        self.playing = None
 
     @commands.command(name='queue', aliases=['q', 'Q', 'Queue'])
     async def queue(self, ctx):
@@ -128,10 +126,17 @@ class Voice(commands.Cog):
             ctx.send("No song is currently playing")
             return
         vc.stop()
+    
+    @commands.command(name='playing', aliases=['Playing'])
+    async def current(self, ctx):
+        if self.playing == None:
+            await ctx.send("No song is currently playing")
+            print("No song is currently playing")
+        else:
+            await ctx.send(f"Currently playing {self.playing}")
+            print(f"Currently playing {self.playing}")
 
-            
 
-
-
-bot.add_cog(Voice(bot))
-bot.run(keys.DISCORD_TOKEN)
+if __name__ == '__main__':
+  bot.add_cog(Music(bot))
+  bot.run(keys.DISCORD_TOKEN)
